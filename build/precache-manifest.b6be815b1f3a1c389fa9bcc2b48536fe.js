d9253e401e68568890e028f464464234fce9a362
Christopher Santelices <christopher.santelices@gmail.com>
2020-06-18 21:21:30 -0400

    init

---
+++ build/precache-manifest.b6be815b1f3a1c389fa9bcc2b48536fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c71878f53d9e413d98ff7c40ffeacab",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "88c89d66d51d0b939168",
+    "url": "/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97dda7dcfbebb3ce8a3c",
+    "url": "/build/static/js/2.e6df987a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.e6df987a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c89d66d51d0b939168",
+    "url": "/build/static/js/main.afd7250e.chunk.js"
+  },
+  {
+    "revision": "bb6fb5ee1b39b248d34c",
+    "url": "/build/static/js/runtime-main.f6bd7341.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
